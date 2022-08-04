9806fde29a3aeb755ea320d3be64e998ef2745c7
alexavierdev <alexavier859@gmail.com>
2022-08-04 20:26:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function madeBoard(size) {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows =`repeat(${size}, 1fr)`
+
+    let amount = size * size
+
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div')
+        square.style.backgroundColor = 'blue'
+        square.style.margin = "1px"
+        board.insertAdjacentElement('beforeend', square)
+    }
+}
+
+
+function changeSize(input){
+    madeBoard(input)
+}
