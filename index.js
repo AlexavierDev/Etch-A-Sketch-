6361a1c7bcbad035b97a181e20f8031ef4a82055
alexavierdev <alexavier859@gmail.com>
2022-08-05 15:38:56 -0300

    create functions changeColor,ChangeSize,colorSquare e reset

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
+let color = 'black'
+let board = document.querySelector(".board")
+let squares = board.querySelectorAll("div")
+
+
 function madeBoard(size) {
-    let board = document.querySelector(".board");
-    let squares = board.querySelectorAll("div");
-    squares.forEach((div) => div.remove());
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    board.style.gridTemplateRows =`repeat(${size}, 1fr)`
+
+
+
+    squares.forEach((div) => div.remove())
+
+
+    board.style.gridTemplateColumns = `repeat(${size},1fr)`
+    board.style.gridTemplateRows = `repeat(${size},1fr)`
 
     let amount = size * size
 
-    for (let i = 0; i < amount; i++) {
-        let square = document.createElement('div')
-        square.style.backgroundColor = 'blue'
-        square.style.margin = "1px"
+    for (i = 0; i < amount; i++) {
+        let square = document.createElement("div")
+        square.addEventListener("mouseover", colorSquare)
         board.insertAdjacentElement('beforeend', square)
     }
 }
 
+madeBoard(16)
 
-function changeSize(input){
-    madeBoard(input)
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        madeBoard(input)
+    } else {
+        alert("type one value greater that 2 or less that 100")
+    }
+
+}
+
+function colorSquare() {
+    if (color == "random") {
+        this.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16)
+    } else {
+        this.style.backgroundColor = color
+    }
+}
+
+function changeColor(choice) {
+    color = choice
+}
+
+function reset() {
+    let board = document.querySelector(".board")
+    let squares = board.querySelectorAll("div")
+    squares.forEach((div) => (div.style.backgroundColor = "white"));
 }
